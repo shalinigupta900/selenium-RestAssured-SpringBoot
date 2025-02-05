73e42e31d8ceb51f41ac362f781805fef8d06c30
shalinigupta900 <shalinigupta900@gmail.com>
2025-02-05 17:11:53 +0530

    first commit

--- src/main/java/driver.java
+++ src/main/java/driver.java
@@ -1,14 +1,20 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class driver
-{
-    public static void main(String...args)
-    {
-//        System.setProperty("webdriver.chrome.driver","C:\\Users\\shalini.gupta\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-//        WebDriver driver=new ChromeDriver();
+public class driver {
+    static int count = 0;
+
+    public static void main(String... args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\shalini.gupta\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.fedex.com/en-in/home.html");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.className("a.fxg-search-icon.js-fxg-search-btn")).click();
+
 
 //        System.setProperty("webdriver.gecko.driver","C:\\Users\\shalini.gupta\\Downloads\\geckodriver-v0.35.0-win64\\geckodriver.exe");
 //       WebDriver firefoxdriver=new FirefoxDriver();
@@ -21,12 +27,45 @@ public class driver
 //        firefoxdriver.close();
 //        firefoxdriver.quit();
 
-        System.setProperty("webdriver.edge.driver","C:\\Users\\shalini.gupta\\Downloads\\edgedriver_win64\\msedgedriver.exe");
-        WebDriver edgedriver=new EdgeDriver();
+//        System.setProperty("webdriver.edge.driver","C:\\Users\\shalini.gupta\\Downloads\\edgedriver_win64\\msedgedriver.exe");
+//        WebDriver edgedriver=new EdgeDriver();
+//
+//
+//        String name= edgedriver.getTitle();
+//        System.out.println(name);
 
-
-        String name= edgedriver.getTitle();
+        driver.get("https://www.fedex.com/en-in/open-account.html");
+        String name = "shalini";
         System.out.println(name);
 
+        char[] value = name.toCharArray();
+        int m = value.length;
+        System.out.println(m);
+
+        for (int i = 0; i < value.length; i++) {
+            for (int j = i + 1; j < value.length; j++) {
+
+                System.out.println(value[i]);
+
+                    System.out.println("----------------");
+                    System.out.println(value[j]);
+
+                }
+
+            }
+          //  System.out.println(value[i]);
+        }
+
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
